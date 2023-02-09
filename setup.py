c63539a562c7d298965c257541ca5ec1f12af02c
Jakub Hertyk <jakub.hertyk@auth0.com>
2023-02-09 10:20:43 -0500

    Generate the setup files for rolling a distribution

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='importrr',
+    version='0.1.0',
+    packages=['importrr'],
+    package_dir={'': 'src'},
+    url='https://github.com/curfew-marathon/importrr',
+    license='GNU',
+    author='curfew-marathon',
+    author_email='curfew-marathon',
+    description='Importrr'
+)
