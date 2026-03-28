530ffd2aaf56d776a0d8a7e23e88c44783f0bcc8
curfew-marathon <98830296+curfew-marathon@users.noreply.github.com>
2026-03-28 17:57:52 +0000

    test: add test for backfill_video_tag in exifhelper
    Adds a parametrized test for `backfill_video_tag` in `tests/test_exifhelper.py` to ensure the ExifTool parameters correctly interpolate the target tags.

--- tests/test_exifhelper.py
+++ tests/test_exifhelper.py
@@ -22,3 +22,27 @@ def test_adjust_extensions_params(mock_run_exiftool):
     ]
 
     mock_run_exiftool.assert_called_once_with(root_dir, expected_params)
+
+
+@patch('src.importrr.exifhelper.run_exiftool')
+@pytest.mark.parametrize("tag", ["CreationDate", "CreateDate"])
+def test_backfill_video_tag_params(mock_run_exiftool, tag):
+    import_dir = '/test/import/dir'
+    root_dir = '/test/root/dir'
+
+    from src.importrr.exifhelper import backfill_video_tag
+    backfill_video_tag(import_dir, root_dir, tag)
+
+    expected_params = [
+        '-overwrite_original',
+        '-datetimeoriginal<' + tag,
+        '-time:all<$' + tag,
+        '-if',
+        'not $datetimeoriginal',
+        '-ext', '3GP',
+        '-ext', 'MOV',
+        '-ext', 'MP4',
+        import_dir
+    ]
+
+    mock_run_exiftool.assert_called_once_with(root_dir, expected_params)
